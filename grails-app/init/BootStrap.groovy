c22b34d9de46ce5f56af435662088a2778c02134
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-10-22 17:02:10 +0100

    Added initial values when in dev mode.

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,22 @@
+import com.xavierlepretre.grailsdemo.Report
+import com.xavierlepretre.grailsdemo.User
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if(Environment.current == Environment.DEVELOPMENT) {
+            def user1 = new User(name: "John", locale: new Locale("en_GB"))
+            user1.save(flush: true)
+
+            def report1 = new Report(author: user1)
+            report1.save(flush: true)
+
+            def user2 = new User(name: "Jean", locale: new Locale("fr_FR"))
+            user2.save(flush: true)
+        }
     }
+
     def destroy = {
     }
 }
